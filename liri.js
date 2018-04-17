51b6c9e4000b6a8fb289b13b7d476ad0db67ba47
David Perez <david.perez@us.abb.com>
2018-04-17 18:38:45 -0400

    adding omdb request

--- liri.js
+++ liri.js
@@ -6,8 +6,8 @@ var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var Twitter = require("twitter");
 var client = new Twitter(keys.twitter);
-var Omdb = require('omdb');
-var omdb = new Omdb(keys.omdb);
+// var Omdb = require('omdb');
+// var omdb = new Omdb(keys.omdb);
 
 var fs = require('fs');
 // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
@@ -39,91 +39,64 @@ if (process.argv[2] === 'my-tweets') {
 else if (process.argv[2] === 'spotify-this-song') {
     var allLines = fs.readFileSync('random.txt').toString().split('\n');
     console.log('allLines: ' + allLines);
-    var song = process.argv[3];
-    if (song !== '') {
-        spotify
-            .search({ type: 'track', query: 'All the Small Things', limit: 1 })
-            .then(function (data, response) {
-                // console.log(JSON.stringify(data, null, 2));
-                console.log('length: ' + data.tracks.items.length);
-                for (let i = 0; i < data.tracks.items.length; i++) {
-                    const element = data.tracks.items[i];
-                    console.log('Artist(s): ' + element.album.artists[0].name);
-                    console.log("The song's name is: " + element.name);
-                    console.log('A preview link of the song from Spotify is: ' + element.preview_url);
-                    console.log('The album name is: ' + element.album.name);
-                    fs.appendFileSync("random.txt", 'spotify-this-song' + ', ' + element.name + "\n");
-                }
-            })
-            .catch(function (err) {
-                console.log('Error occurred: ' + err);
-            }); 
+    var song = '';
+    if (process.argv[3] !== '') {
+        song = process.argv[3];
     }
     else {
-        spotify
-            .search({ type: 'track', query: 'The Sign', limit: 1 })
-            .then(function(data, response){
-                const element = data.tracks.items[0];
+        song = 'The Sign'
+    }
+    spotify
+        .search({ type: 'track', query: 'All the Small Things', limit: 1 })
+        .then(function (data, response) {
+            // console.log(JSON.stringify(data, null, 2));
+            console.log('length: ' + data.tracks.items.length);
+            for (let i = 0; i < data.tracks.items.length; i++) {
+                const element = data.tracks.items[i];
                 console.log('Artist(s): ' + element.album.artists[0].name);
                 console.log("The song's name is: " + element.name);
                 console.log('A preview link of the song from Spotify is: ' + element.preview_url);
                 console.log('The album name is: ' + element.album.name);
                 fs.appendFileSync("random.txt", 'spotify-this-song' + ', ' + element.name + "\n");
-            })
-            .catch(function(err){
-                console.log('Error occurred: ' + err);
-            })
-    }
+            }
+        })
+        .catch(function (err) {
+            console.log('Error occurred: ' + err);
+        }); 
 }
 else if (process.argv[2] === 'movie-this') {
+    var info = {};
     // API URL: http://www.omdbapi.com/?i=tt3896198&apikey=omdb
+
     if (process.argv[3] !== '') {
-        omdb.search('saw', function (err, movies) {
-            if (err) {
-                return console.error(err);
-            }
-
-            if (movies.length < 1) {
-                return console.log('No movies were found!');
-            }
-
-            movies.forEach(function (movie) {
-                // * Title of the movie.
-                // * Year the movie came out.
-                // * IMDB Rating of the movie.
-                // * Rotten Tomatoes Rating of the movie.
-                // * Country where the movie was produced.
-                // * Language of the movie.
-                // * Plot of the movie.
-                // * Actors in the movie.
-
-                console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
-                console.log('Rooten Tomatoes %d/10', movie.tomatoRating);
-                console.log('country(%s) and language(%d)', movie.Country, movie.language);
-                console.log(movie.plot);
-                console.log('actors: ', movie.Actors);
-            });
-        });
-    } else {
-        omdb.get({ title: 'Mr. Nobody.', year: 2009 }, true, function (err, movie) {
-            if (err) {
-                return console.error(err);
-            }
-
-            if (!movie) {
-                return console.log('Movie not found!');
-            }
-
-            console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
-            console.log('Rooten Tomatoes %d/10', movie.tomatoRating);
-            console.log('country(%s) and language(%d)', movie.Country, movie.language);
-            console.log(movie.plot);
-            console.log('actors: ', movie.Actors);
-
-            // Mr. Nobody. (2009) 7.9/10
-            // Mr. Nobody tells the life story of Nemo...
-        });
+        info = {
+            title: process.argv[3]
+        };
     }
+    else {
+        info = {
+            title: 'Mr. Nobody.', 
+            year: 2009
+        };
+    }
+
+    var queryURL = "https://www.omdbapi.com/?t=" + info.title + "&y=&plot=short&apikey=" + keys.omdb.apikey;
+    // console.log('keys: ' + keys.omdb);
+    request(queryURL, function (error, response, body) { 
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received  
+        // if (response.statusCode === '200') {
+        console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+        if (error) {
+            return console.error(error);
+        }
+        var movie = JSON.parse(body);
+
+        console.log('%s (%d) %d/10', movie.Title, movie.Year, movie.imdbRating);
+        console.log('Rooten Tomatoes %s', movie.Ratings[1]['Value']);
+        console.log('Country(%s) and language(%s)', movie.Country, movie.Language[0]);
+        console.log(movie.Plot);
+        console.log('actors: ', movie.Actors);
+    });
 }
 else if (process.argv[2] === 'do-what-it-says') {
     
